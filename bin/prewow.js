5d83045e2f8f628a8313c392d1e644302adf75c5
Niels Mentink <niels.mentink@outlook.com>
2016-12-13 20:37:17 +0100

    Added start of installing new parsers

--- bin/prewow.js
+++ bin/prewow.js
@@ -1,40 +1,35 @@
 #!/usr/bin/nodejs
 
 const prewow = require("../lib/prewow");
-const program = require("commander"); // Easier commandline options
 const path = require('path');
 const argparse = require('argparse').ArgumentParser;
 
 var file;
 
-// // Set commandline options
-// program.version('0.0.1');
-// program.usage('[options] <file ...>');
-// program.arguments('<afile>');
-// program.option('-a, --aerbose', 'Output developer messages');
-// program.action(function(afile) {
-//   file = path.resolve(afile);
-// });
-// program.parse(process.argv);
-
 var parser = new argparse({
-  version: '0.0.1',
+  version: require('../package.json').version,
   addHelp:true,
+  prog: 'prewow',
   description: 'Prewow, is an easy way to preview different kinds of files'
 });
 parser.addArgument(
-  [ '-V', '--verbose' ],
+  '--verbose',
   {
-    help: 'Enable verbose output'
+    help: 'Enable verbose output',
+    action: 'storeTrue'
   }
 );
+parser.addArgument(
+  ['-i', '--install'],
+  {
+    help: 'Install new parsers',
+  }
+)
 parser.addArgument(
   'file',
   {
     help: 'The file to parse'
   }
 );
-var args = parser.parseArgs();
-console.dir(args);
 
-prewow(path.resolve(args.file));
+prewow(JSON.stringify(parser.parseArgs()));
