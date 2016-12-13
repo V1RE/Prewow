b6064c2163530f26530679a6059130cdddfba05d
Niels Mentink <niels.mentink@outlook.com>
2016-12-13 01:32:28 +0100

    Fixed lib/prewow, verboselog not working currently

--- bin/prewow.js
+++ bin/prewow.js
@@ -1,1 +1,18 @@
-#!/usr/bin/env node
+#!/usr/bin/env nodejs
+const prewow = require("../lib/prewow");
+const program = require("commander"); // Easier commandline options
+const path = require('path');
+
+var file;
+
+// Set commandline options
+program.version('0.0.1');
+program.usage('[options] <file ...>');
+program.arguments('<afile>');
+program.option('-v, --verbose', 'Output developer messages');
+program.action(function(afile) {
+  file = path.resolve(afile);
+});
+program.parse(process.argv);
+
+prewow(file);
