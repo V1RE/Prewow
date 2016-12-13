55ae770a788de2903388a3988e24c9542e90d916
Niels Mentink <niels.mentink@outlook.com>
2016-12-13 22:22:49 +0100

    Added some semicolons and beautified all files

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Prewow
+
 An easy way to preview html, markdown and more!
 
 # Features
-* Multiple Markdown themes
-* Emoji shortcodes :gun:
+
+-   Multiple Markdown themes
+-   Emoji short-codes :gun:
 
 # TODO
-- [x] Make server
-- [x] Detect filetype
+
+-   [x] Make server
+-   [x] Detect filetype
--- bin/prewow.js
+++ bin/prewow.js
@@ -8,26 +8,23 @@ var file;
 
 var parser = new argparse({
   version: require('../package.json').version,
-  addHelp:true,
+  addHelp: true,
   prog: 'prewow',
   description: 'Prewow, is an easy way to preview different kinds of files'
 });
 parser.addArgument(
-  '--verbose',
-  {
+  '--verbose', {
     help: 'Enable verbose output',
     action: 'storeTrue'
   }
 );
 parser.addArgument(
-  ['-i', '--install'],
-  {
-    help: 'Install new parsers',
+  ['-i', '--install'], {
+    help: 'Install new parsers'
   }
-)
+);
 parser.addArgument(
-  'file',
-  {
+  'file', {
     help: 'The file to parse'
   }
 );
